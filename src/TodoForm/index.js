7d985fbd5b5d27551979b7890396dc44ef417aed
prodasl <prodasl@hotmail.com>
2024-10-29 14:59:25 +0700

    First version

---
+++ src/TodoForm/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import "./TodoForm.css";
+
+function TodoForm() {
+    const {
+        addTodo,
+        setOpenModal,
+    } = React.useContext(TodoContext);
+
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
+
+    const onCancel = () => {
+        setOpenModal(false);    
+    };
+
+    const onChange = (event) => {
+        setNewTodoValue(event.target.value)
+    };
+
+    return(
+    <form onSubmit={onSubmit}>
+        <label>Enter your new TODO</label>
+        <textarea placeholder="Cut onion for the lunch" 
+        value={newTodoValue}
+        onChange={onChange}
+        />
+        <div className="TodoForm-buttonContainer">
+            <button 
+            type="button"
+            className="TodoForm-button
+            TodoForm-button--cancel"
+            onClick={onCancel}>
+                Cancel
+            </button>
+            <button 
+            type="submit"
+            className="TodoForm-button
+            TodoForm-button--add">
+                Add
+            </button>
+        </div>
+    </form>
+    );
+}
+
+export { TodoForm };
